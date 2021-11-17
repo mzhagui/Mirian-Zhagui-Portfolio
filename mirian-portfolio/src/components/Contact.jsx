abbc064b2455e4c7d7c33f07841fc6f0e057a67a
Mirian Zhagui <mirianzhagui@Mirians-MacBook-Air.local>
2021-11-16 21:31:03 -0500

    contact me

--- mirian-portfolio/src/components/Contact.jsx
+++ mirian-portfolio/src/components/Contact.jsx
@@ -1,8 +1,7 @@
-import { MailIcon } from '@heroicons/react/solid'
 
 export default function Contact() {
   return (
-    <section id="contact-me">
+    <section id="contact-me" className="bg-white">
      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
@@ -11,17 +10,19 @@ export default function Contact() {
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
 Thanks for visiting my page, for more information or to contact me please do so via:          </p>
         </div>
+        <div className="flex justify-between">
 <button className="far fa-envelope fa-5x"
           onClick={() => {
           window.location.href = `mailto:mzhagui29@gmail.com?subject=&body= Hello`
           }} > 
         </button>
-        <a href="https://github.com/mzhagui" target="_blank">
-<img className="h-20 w-20" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>
+        <a href="https://github.com/mzhagui" target="_blank" >
+<img className="h-20 w-20	" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>
         </a>
         <a href="https://www.linkedin.com/in/mirian-zhagui-79284421a/" target="_blank">
 <img className="h-20 w-20" src="https://cdn3.iconfinder.com/data/icons/inficons/512/linkedin.png"/>
         </a>
+        </div>
     </div>
     </section>
   )
--- mirian-portfolio/src/components/Navbar.jsx
+++ mirian-portfolio/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         text-gray-900
         mb-4
         md:mb-0"> </a>
-        <a href="#about" className="ml-3 text-xl text-white">
+        <a href="#about" className="ml-3 text-4xl text-white">
         Mirian Zhagui</a>
       <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
         <a href="#projects" className="mr-5 text-white hover:underline text-left">
